f2b3ce4d0957998ec705fbfce49f22d88978bf51
Javier Jesús Martínez Moreno <jj_dev@outlook.com>
2025-01-14 11:43:48 -0600

    feat: add extension's simulation from fakes
    create a configurable fake method to throw an exception

--- LogAn.UnitTests/ch3/LogAnalyzerTests.cs
+++ LogAn.UnitTests/ch3/LogAnalyzerTests.cs
@@ -13,12 +13,29 @@ namespace LogAn.ch3.UnitTests {
 
             bool result = logAn.IsValidLogFileName("short.ext");
         }
+
+        [Test]
+        public void IsValidFileName_ExtManagerThrowsException_ReturnsFalse() {
+            FakeExtensionManager myFakeManager = new FakeExtensionManager();
+            myFakeManager.WillThrow = new Exception("this is fake");
+
+            LogAnalyzer logAn = new LogAnalyzer(myFakeManager);
+            bool result = logAn.IsValidLogFileName("anything.anyextension");
+            Assert.False(result);
+        }
     }
 
     internal class FakeExtensionManager : IExtensionManager {
         public bool WillBeValid = false;
 
+        // convert `exception` to a maybe-null type to get rid of warning CS8625
+        public Exception? WillThrow = null;
+
         public bool IsValid(string fileName) {
+            if (WillThrow != null) {
+                throw WillThrow;
+            }
+
             return WillBeValid;
         }
     }
